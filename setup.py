c7f292eb11e0a3694b4d09746d59281fb0b4a0eb
Bruno Binet <binet.bruno@gmail.com>
2012-01-27 23:36:39 +0100

    set version to 0.2dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'python-archive',
-    version = '0.1',
+    version = '0.2dev',
     author = 'Gary Wilson Jr.',
     author_email = 'gary.wilson@gmail.com',
     packages = ['archive', 'archive.test'],
