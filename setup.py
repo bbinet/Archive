5e4cbca38700b4c096a0a4cfba84989e736532c8
Bruno Binet <bruno.binet@gmail.com>
2012-02-02 21:59:26 +0100

    prepare 0.2 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'Archive',
-    version = '0.2dev',
+    version = '0.2',
     author = 'Gary Wilson Jr.',
     author_email = 'gary.wilson@gmail.com',
     packages = find_packages(),
