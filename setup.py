d57feac9d896fc50b5f97dc3a7a79b8f1af67cae
Bruno Binet <bruno.binet@gmail.com>
2012-02-06 22:54:53 +0100

    update setup.py with maintainer and url metadata

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version = '0.4dev',
     author = 'Gary Wilson Jr.',
     author_email = 'gary.wilson@gmail.com',
+    maintainer = 'Bruno Binet',
+    maintainer_email = 'bruno.binet@gmail.com',
     packages = find_packages(),
-    url = 'http://code.google.com/p/python-archive/',
+    url = 'https://github.com/bbinet/Archive',
     license = 'LICENSE.txt',
     description = ('Simple library that provides a common interface for'
                    ' extracting zip and tar archives.'),
